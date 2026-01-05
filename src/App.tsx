a50b6289ca866ab465d6948ef35ce1110d30cffc
Fallingfl <56891375+Fallingfl@users.noreply.github.com>
2026-01-06 05:26:38 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,15 @@ export function App() {
     }, []);
     
     return <div class="container max-w-screen-sm px-3">
-        <h1 class="text-3xl ff-heading font-black mt-3 text-center">Escape from Tarkov</h1>
-        <h2 class="text-2xl xl:text-3xl ff-heading font-black text-center mb-2">Real time to ingame time</h2>
+        <h1 class="text-3xl ff-heading font-black mt-3 text-center">逃离塔科夫</h1>
+        <h2 class="text-2xl xl:text-3xl ff-heading font-black text-center mb-2">实际时间到游戏内时间</h2>
         <div class="grid grid-cols-2 grid-rows-1">
             <Scroller isLeft={true} time={time} />
             <Scroller isLeft={false} time={time} />
         </div>
         <div class="z-50 relative text-xs text-gray-500 text-right mt-2">
-            <div>Time in Escape from Tarkov moves at 7 seconds per second.</div>
-            <a href="https://github.com/adamburgess/tarkov-time">Source code at github.com/adamburgess/tarkov-time</a>
+            <div>逃离塔科夫中的时间流逝速度为每秒 7 秒。</div>
+            <a href="https://github.com/adamburgess/tarkov-time">鸣潮启动</a>
         </div>
     </div>
 }
